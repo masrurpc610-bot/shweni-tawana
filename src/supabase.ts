d419590185b0697ace9027e646a151c4b3ef7332
Masrour <m4sruur@gmail.com>
2026-07-26 08:19:01 +0300

    fix url

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rgggjdizzykgblnmgvt.supabase.co'
+const supabaseUrl = 'https://rgggjdizzykglblnmgvt.supabase.co'
 const supabaseKey = 'sb_publishable_R69U888pYo1efvY-AewK4w_LzkRZAQS'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
